1b18180161e9da311ccbc5dc68e1b7cec330630e
carldamey <carldamey@gmail.com>
2023-12-04 14:04:44 -0800

    finishes notes by completing lesson 3

--- python-classes.py
+++ python-classes.py
@@ -1,18 +1,47 @@
 class Cat():
+  #class attribute
+  next_id = 1
+
   def __init__(self, name, age=0):
     self.name = name
     self.age = age
+    self.id = Cat.next_id
+    Cat.next_id += 1
 
   def meow(self):
     print(f"{self.name} says meow!")
 
   def __str__(self):
-    return f"{self.name} is {self.age} years old!"
+    return f"Cat {self.id} named {self.name} is {self.age} years old!"
 
-bug = Cat("Bug", 2)
+  # decorator
+  @classmethod
+  def get_total_cats(cls):
+    return cls.next_id - 1
+
+
+# class inheritance
+class dumbCat(Cat):
+  # add additional parameters AFTER the ones in the superclass
+  def __init__(self, name, age=0, iq=0):
+    # always call the superclass' __init__ first
+    Cat.__init__(self, name, age)
+    # now add any new attributes
+    self.iq = iq
+
+  def lower_iq(self, amount):
+    self.iq -= amount
+    print(f"{self.name}'s iq is now {self.iq}.")
+
+bug = dumbCat("Bug", 2, 4)
+pooka = Cat("Pooka", 8)
 print(bug)
 bug.meow()
-
+print(pooka)
+pooka.meow()
+print(Cat.get_total_cats())
+bug.lower_iq(35)
+print(bug.iq)
 
 class Vehicle():
   def __init__(self, make, model, running=False):
