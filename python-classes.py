40b2c6a82e1b11b9d5307bafe9a843f57de72354
carldamey <carldamey@gmail.com>
2023-12-04 11:49:17 -0800

    finishes lesson 2 notes

--- python-classes.py
+++ python-classes.py
@@ -10,6 +10,31 @@ class Cat():
     return f"{self.name} is {self.age} years old!"
 
 bug = Cat("Bug", 2)
-
 print(bug)
 bug.meow()
+
+
+class Vehicle():
+  def __init__(self, make, model, running=False):
+    self.make = make
+    self.model = model
+    self.running = running
+  
+  def start(self):
+    self.running = True
+    print("Running...")
+
+  def stop(self):
+    self.running = False
+    print("Stopped...")
+
+  def __str__(self):
+    return f"Vehicle is a {self.make} {self.model}."
+
+car = Vehicle("Toyota", "Solara")
+print(car)
+print(car.running)
+car.start()
+print(car.running)
+car.stop()
+print(car.running)
